37dcf8136a9fa735a737ecc2f67c38a1c89a7ff4
Ibbi Khan <ibbikhan@Internet.local>
2019-01-16 19:57:17 +0500

    Setup user location on map

--- PinPoint/Pins/PinsViewController.swift
+++ PinPoint/Pins/PinsViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
-class PinsViewController: UIViewController {
-    
+class PinsViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager = CLLocationManager()
     var defaultLocation = CLLocation(latitude: 33.5889, longitude: 71.4429)
     var mapView: GMSMapView!
     let zoomLevel: Float = 15.0
@@ -18,10 +18,47 @@ class PinsViewController: UIViewController {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
         
+        // givig defualt location
+        setupMap()
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            switch CLLocationManager.authorizationStatus() {
+            case .notDetermined, .restricted, .denied:
+                print("No access")
+            case .authorizedAlways, .authorizedWhenInUse:
+                print("Access")
+            }
+        }else{
+            print("location is disable")
+        }
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+    }
+    func setupMap() {
         let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude, longitude: defaultLocation.coordinate.longitude, zoom: zoomLevel)
-        let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height), camera: camera)
+        mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height), camera: camera)
+        mapView.isMyLocationEnabled = true
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(mapView)
     }
-
+    
+    // delegates properties
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: zoomLevel)
+        mapView.camera = camera
+        // setting up marker for user.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        marker.map = mapView
+    }
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
+    }
 }
