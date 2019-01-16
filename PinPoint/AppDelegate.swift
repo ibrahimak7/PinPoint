5c9b3c9334cde4320d6ff2c704852d5b86b3f3dc
Ibbi Khan <ibbikhan@Internet.local>
2019-01-16 07:36:55 +0500

    google map added

--- PinPoint/AppDelegate.swift
+++ PinPoint/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import GoogleMaps
+import GooglePlaces
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GMSServices.provideAPIKey("AIzaSyCoWuRglYJb1otJJK5T_yDyn1n_CbpHKuo")
+        GMSPlacesClient.provideAPIKey("AIzaSyCoWuRglYJb1otJJK5T_yDyn1n_CbpHKuo")
         return true
     }
 
--- PinPoint/Pins/PinsViewController.swift
+++ PinPoint/Pins/PinsViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
-
+import GoogleMaps
+import GooglePlaces
 class PinsViewController: UIViewController {
-
+    
+    var defaultLocation = CLLocation(latitude: 33.5889, longitude: 71.4429)
+    var mapView: GMSMapView!
+    let zoomLevel: Float = 15.0
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
-        // Do any additional setup after loading the view.
+        
+        let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude, longitude: defaultLocation.coordinate.longitude, zoom: zoomLevel)
+        let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height), camera: camera)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(mapView)
     }
-    
 
 }
