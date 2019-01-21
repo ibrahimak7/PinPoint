8132c6db3290b2eb23281ada1a6ec9bf01a108fd
Ibbi Khan <kkibrahim008@gmail.com>
2019-01-21 20:41:33 +0500

    methods created to fetch new users

--- PinPoint/Search/Presenter/SearchPresenter.swift
+++ PinPoint/Search/Presenter/SearchPresenter.swift
@@ -12,29 +12,43 @@ class SearchPresenter: NSObject {
     var delegate: SearchProtocol!
     var ref: DatabaseReference!
     var usersList = [SearchModel]()
-    
+    let uid = Auth.auth().currentUser?.uid
+    var familyMembers = [String]()
     func findUser(userName name: String) {
         usersList.removeAll()
         configDB()
         ref.child("Profile").observeSingleEvent(of: .value) { (snapShot) in
             if snapShot.exists() {
-                let data = snapShot.value as? NSDictionary
-                let keys = data?.allKeys
-                for key in keys! {
-                    let user = data![key] as? NSDictionary
-                    if name == user!["name"]! as? String {
-                        let name = user!["name"]! as? String
-                        self.usersList.append(SearchModel(name: name!, id: key as! String))
+                let dataP = snapShot.value as? NSDictionary
+                let keys = dataP?.allKeys
+                self.ref.child("Family/\(self.uid!)").observeSingleEvent(of: .value, with: { (snapShot2) in
+                    if snapShot2.exists() {
+                        let data = snapShot2.value as? NSDictionary
+                        self.familyMembers = data?.allKeys as! [String]
+                        self.extractNewUsers(familyMembers: self.familyMembers, keys: (keys as? [String])!, data: dataP!, name: name)
                     }
-                }
-                self.delegate.searchComplete(searchDataFetched: self.usersList)
+                })
+                
             }
         }
+    }
+    func extractNewUsers(familyMembers: [String], keys: [String], data: NSDictionary, name: String) {
+        for key in keys {
+            print(key)
+            if !familyMembers.contains(key) {
+                let user = data[key] as? NSDictionary
+                if name == user!["name"]! as? String {
+                    let name = user!["name"]! as? String
+                    self.usersList.append(SearchModel(name: name!, id: key))
+                }
+            }
+        }
+        self.delegate.searchComplete(searchDataFetched: self.usersList)
     }
     func getAllRequests(){
         let uid = Auth.auth().currentUser?.uid
         configDB()
-        ref.child("Requests/\(uid!)").observeSingleEvent(of: .value) { (snapShot) in
+        ref.child("Famaily/\(uid!)").observeSingleEvent(of: .value) { (snapShot) in
             if snapShot.exists() {
                 
             }
