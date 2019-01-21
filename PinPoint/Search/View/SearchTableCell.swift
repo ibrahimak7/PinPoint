0af8d2a8855c687e3d21d21fc840774b9554e9b2
Ibbi Khan <ibbikhan@Internet.local>
2019-01-21 15:15:30 +0500

    remove extra code from searchtableCell

--- PinPoint/Search/View/SearchTableCell.swift
+++ PinPoint/Search/View/SearchTableCell.swift
@@ -19,10 +19,4 @@ class SearchTableCell: UITableViewCell {
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
